f92115ce7506d32efaee4457aae27b7590c791ef
Keshav Sreekantham <ksreekan@purdue.edu>
2025-07-06 19:41:11 -0500

    Week 3 Matches and leaderboard updates

--- calculation/leaderboard_updater.py
+++ calculation/leaderboard_updater.py
@@ -1,4 +1,4 @@
-from script import calculate_match_elos, read_elos
+from script import calculate_match_elos, read_elos,calculate_wins
 from typing import Dict
 
 def is_integer(s):
@@ -48,7 +48,34 @@ def read_official_matches(elo_dict: Dict[str,int], matches_txt = "matches.txt", 
 if __name__ == "__main__":
     elo_dict = {}
     read_elos(elo_dict)
+    print("PERFORMANCE ELO GAINS")
     read_official_matches(elo_dict)
+
+    wl_dict = calculate_wins("matches.txt")
+
+    base_multiplier = 1.5
+    print("BASE ELO GAINS")
+    for player,diff in wl_dict.items():
+        if diff == 1:
+            elo_dict[player] += 30 * base_multiplier
+            print(player + ": " + str(30 * base_multiplier))
+        if diff == 2:
+            elo_dict[player] += 33 * base_multiplier
+            print(player + ": " + str(33 * base_multiplier))
+        if diff == 3:
+            elo_dict[player] += 36 * base_multiplier
+            print(player + ": " + str(36 * base_multiplier))
+        if diff == -1:
+            elo_dict[player] += -5 * base_multiplier
+            print(player + ": " + str(-5 * base_multiplier))
+        if diff == -2:
+            elo_dict[player] += -10 * base_multiplier
+            print(player + ": " + str(-10 * base_multiplier))
+        if diff == -3:
+            elo_dict[player] += -15 * base_multiplier
+            print(player + ": " + str(-15 * base_multiplier))
+        round(elo_dict[player])
+
 
     with open("elos.txt", "w") as file:
         for key, value in elo_dict.items():
--- calculation/script.py
+++ calculation/script.py
@@ -4,7 +4,12 @@ from typing import Dict
 
 
 
-
+def is_integer(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 def read_elos(elo_dict: Dict[str, int]):
     with open("elos.txt", "r") as file:
@@ -22,7 +27,7 @@ def read_matches():
 def calculate_match_elos(person1: str, person2: str, p1score: int, p2score: int, elo_dict: Dict[str, int], difference: bool):
 
     #-----------CONFIG-------------------#
-    overall_multiplier = 80
+    overall_multiplier = 40
     score_significance_multiplier= -3.4
     elo_significance_modifier = 400
     #------------------------------------#
@@ -79,6 +84,40 @@ def calculate_match_elos(person1: str, person2: str, p1score: int, p2score: int,
         return p1_diff,p2_diff
     return None
 
+def calculate_wins(matches_txt: str) -> Dict[str, int]:
+    with open(matches_txt, "r") as file:
+        person1 = ""
+        person2 = ""
+        first = True
+        match_differential = 0
+        wl_dict = {}
+        for line in file:
+            if line == "END":
+                wl_dict[person1] = match_differential
+                wl_dict[person2] = -1 * match_differential
+                continue
+            x = line.split(",")
+            if is_integer(x[0]):
+                old_person1 = person1
+                old_person2 = person2
+                person1 = x[1].strip()
+                person2 = x[2].strip()
+                if first:
+                    first = False
+                    continue
+                else:
+                    wl_dict[old_person1] = match_differential
+                    wl_dict[old_person2] = -1 * match_differential
+                    match_differential = 0
+
+            else:
+                if int(x[1]) > int(x[2]):  # person 1 won
+
+                    match_differential += 1
+                if int(x[1]) < int(x[2]):  # person 2 won
+
+                    match_differential -= 1
+    return wl_dict
 
 if __name__ == '__main__':
     elo_dict = {}
